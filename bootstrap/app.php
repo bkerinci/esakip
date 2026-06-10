abdd70f004bc56b2d218c2071c56cb90d6739821
bkerinci <admin@kerinci.go.id>
2026-06-10 23:00:35 +0700

    fix: use trustProxies to correctly detect HTTPS on Vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
